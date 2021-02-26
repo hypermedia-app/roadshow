3719f5f0628beab351000219ae913ccd8d22a74c
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-02-26 15:41:37 +0100

    style: reformat

--- apps/storybook/.build/transformTurtle.js
+++ apps/storybook/.build/transformTurtle.js
@@ -1,27 +1,27 @@
-const {parsers} = require('@rdf-esm/formats-common')
+const { parsers } = require('@rdf-esm/formats-common')
 const toStream = require('string-to-stream')
-const toString =require( 'stream-to-string')
+const toString = require('stream-to-string')
 const Serializer = require('@rdfjs/serializer-rdfjs')
-const { createFilter } =require ('@rollup/pluginutils')
+const { createFilter } = require('@rollup/pluginutils')
 
 const serializer = new Serializer({
-  module: 'esm'
+  module: 'esm',
 })
 
 module.exports = function turtle () {
-  const filter = createFilter(/ttl$/);
+  const filter = createFilter(/ttl$/)
 
   return {
     name: 'turtle-transform',
-    async transform ( code, id ) {
-      if ( !filter( id ) ) return;
+    async transform (code, id) {
+      if (!filter(id)) return
       const stream = await parsers.import('text/turtle', toStream(code))
 
       return {
         code: await toString(serializer.import(stream)),
         map: { mappings: '' },
         moduleSideEffects: false,
-      };
-    }
-  };
+      }
+    },
+  }
 }
--- apps/storybook/web-dev-server.config.mjs
+++ apps/storybook/web-dev-server.config.mjs
@@ -1,5 +1,5 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
-import { storybookPlugin } from '@web/dev-server-storybook';
+import { esbuildPlugin } from '@web/dev-server-esbuild'
+import { storybookPlugin } from '@web/dev-server-storybook'
 import { fromRollup } from '@web/dev-server-rollup'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
@@ -7,34 +7,34 @@ import rdfjs from 'rdfjs-eds-plugin'
 import turtle from './.build/transformTurtle.js'
 
 export default {
-    open: false,
-    nodeResolve: true,
-    rootDir: '../..',
-    plugins: [
-        {
-          transform({ path }) {
-            if (path.endsWith('ttl')) {
-              return {
-                headers: {
-                  'content-type': 'application/javascript'
-                }
-              }
-            }
+  open: false,
+  nodeResolve: true,
+  rootDir: '../..',
+  plugins: [
+    {
+      transform ({ path }) {
+        if (path.endsWith('ttl')) {
+          return {
+            headers: {
+              'content-type': 'application/javascript',
+            },
           }
-        },
-        esbuildPlugin({ ts: true }),
-        storybookPlugin({ type: 'web-components' }),
-        rdfjs,
-        fromRollup(turtle)(),
-        fromRollup(nodeResolve)(),
-        fromRollup(commonjs)({
-            exclude: [
-                '**/node_modules/@open-wc/**/*',
-                '**/node_modules/chai/**/*',
-                '**/node_modules/chai-dom/**/*',
-                '**/node_modules/sinon-chai/**/*',
-                '**/node_modules/@web/**/*',
-            ]
-        }),
-    ],
-};
+        }
+      },
+    },
+    esbuildPlugin({ ts: true }),
+    storybookPlugin({ type: 'web-components' }),
+    rdfjs,
+    fromRollup(turtle)(),
+    fromRollup(nodeResolve)(),
+    fromRollup(commonjs)({
+      exclude: [
+        '**/node_modules/@open-wc/**/*',
+        '**/node_modules/chai/**/*',
+        '**/node_modules/chai-dom/**/*',
+        '**/node_modules/sinon-chai/**/*',
+        '**/node_modules/@web/**/*',
+      ],
+    }),
+  ],
+}
