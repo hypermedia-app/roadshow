c2aae10ec467cd37c9e6e30d2241148a3002b48b
tpluscode <tpluscode@users.noreply.github.com>
2023-07-03 12:20:24 +0200

    styleL unused ignores

--- packages/test-data/index.ts
+++ packages/test-data/index.ts
@@ -1,16 +1,18 @@
-import module from 'module'
+/* eslint-disable no-console */
+import { resolve } from 'path'
+import url from 'url'
 import { createReadStream } from 'fs'
 import { StreamParser } from 'n3'
 import type { Stream } from 'rdf-js'
 
 export function loadData(path: string, base = import.meta.url): Stream | null {
   const parser = new StreamParser({ format: 'text/n3' })
-  const require = module.createRequire(base)
   let fullPath: string
   try {
-    fullPath = require.resolve(`${path}`)
+    fullPath = resolve(url.fileURLToPath(new URL(`${path}.ttl`, base)))
   } catch (e: any) {
     if (e.message.includes('Cannot find module')) {
+      console.error(e)
       return null
     }
 
