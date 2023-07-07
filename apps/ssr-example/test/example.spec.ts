8471751f9a22199f2afd4a54e096cac7129d6574
tpluscode <tpluscode@users.noreply.github.com>
2023-07-07 23:39:46 +0200

    test: playwright

--- apps/ssr-example/test/example.spec.ts
+++ apps/ssr-example/test/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('simple page', async ({ page }) => {
   await page.goto('/example/page')
 
-  const h1 = page.getByText('Test page resource')
+  const h1 = page.locator('h1')
 
   await expect(h1).toHaveAttribute('slot', 'header')
 })
